2f04c266c94e013536503024581e30de8ecc2f8e
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-04-19 11:41:22 +0900

    Title: 상품 코드, Time: 170ms, Memory: 24MB, Status: Passed - CodeTree

--- 240419/상품 코드/product-code.py
+++ 240419/상품 코드/product-code.py
@@ -7,5 +7,8 @@ class Box:
 
 name, code = sys.stdin.readline().split()
 code = int(code)
+sample = Box("codetree", 50)
 box = Box(name, code)
+
+print(f"product {sample.code} is {sample.name}")
 print(f"product {box.code} is {box.name}")
