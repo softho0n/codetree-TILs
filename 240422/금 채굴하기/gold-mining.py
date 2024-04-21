f05f892650f701b4f74c7312a41d41d4c8612951
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-04-22 01:49:08 +0900

    Title: 금 채굴하기, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240422/금 채굴하기/gold-mining.py
@@ -0,0 +1,44 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, sys.stdin.readline().split())))
+
+answer = 0
+
+def calc(i, j, k):
+    sample = []
+
+    step = 0
+    for y in range(i, i+k+1):
+        for x in range(j-k+step, j+k-step+1):
+            sample.append((y, x))
+        step += 1
+    
+    step = 1
+    for y in range(i-1, i-k-1, -1):
+        for x in range(j-k+step, j+k-step+1):
+            sample.append((y, x))
+        step += 1
+
+    tmp_cnt = 0
+    for y, x in sample:
+        if y < 0 or x < 0 or y >= n or x >= n:
+            continue
+        
+        if graph[y][x] == 1:
+            tmp_cnt += 1
+    
+    total_value = k * k + (k + 1) * (k + 1)
+    credit = tmp_cnt * m
+    if credit >= total_value:
+        global answer
+        answer = max(answer, tmp_cnt)
+
+for k in range(n // 2 + 1):
+    for i in range(n):
+        for j in range(n):
+            calc(i, j, k)
+
+print(answer)
