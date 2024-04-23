415c3afcb18cccc19e79e04ac91c8c25bfa0b066
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-04-24 01:56:24 +0900

    Title: 마을 구분하기, Time: 156ms, Memory: 25MB, Status: Passed - CodeTree

--- 240424/마을 구분하기/seperate-village.py
+++ 240424/마을 구분하기/seperate-village.py
@@ -15,10 +15,7 @@ dy = [1, -1, 0, 0]
 dx = [0, 0, 1, -1]
 tmp_answer = 0
 
-def dfs(y, x, cnt):
-    global tmp_answer
-    tmp_answer = max(tmp_answer, cnt)
-
+def dfs(y, x):
     for k in range(4):
         ny = y + dy[k]
         nx = x + dx[k]
@@ -30,18 +27,21 @@ def dfs(y, x, cnt):
             continue
 
         if graph[ny][nx] == 1:
+            global tmp_answer
             visited[ny][nx] = True
-            dfs(ny, nx, cnt + 1)
+            tmp_answer += 1
+            dfs(ny, nx)
+    
+    return tmp_answer
 
 cnt = 0
 sample = []
 for i in range(n):
     for j in range(n):
         if graph[i][j] == 1 and visited[i][j] is False:
-            tmp_answer = 0
+            tmp_answer = 1
             visited[i][j] = True
-            dfs(i, j, 1)
-            sample.append(tmp_answer)
+            sample.append(dfs(i, j))
             cnt += 1
 
 sample.sort()
