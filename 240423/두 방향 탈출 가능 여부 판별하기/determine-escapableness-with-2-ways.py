b8a78d543d643c8ae63377dfc190c9c79a5f2efa
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-04-23 10:10:42 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240423/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240423/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -9,7 +9,7 @@ dy = [0, 0, 1, -1]
 dx = [1, 1, 0, 0]
 
 visited = [
-    [False for _ in range(n+1)]
+    [False for _ in range(m+1)]
     for _ in range(n+1)
 ]
 
@@ -17,25 +17,19 @@ def dfs(sy, sx):
     for k in range(4):
         ny, nx = sy + dy[k], sx + dx[k]
 
-        if ny < 0 or nx < 0 or ny >= n or nx >= n:
+        if ny < 0 or nx < 0 or ny >= n or nx >= m:
             continue
 
-        if visited[ny][nx]:
-            continue
-        
-        if graph[ny][nx] == 0:
-            continue
-        
-        visited[ny][nx] = True
-        dfs(ny, nx)
-        visited[ny][nx] = False
-        
+        if visited[ny][nx] is False and graph[ny][nx] == 1:
+            visited[ny][nx] = True
+            dfs(ny, nx)
+            visited[ny][nx] = False
 
 
 visited[0][0] = True
 dfs(0, 0)
 
-if visited[n-1][n-1]:
+if visited[n-1][m-1]:
     print(1)
 else:
     print(0)
