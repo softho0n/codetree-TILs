89d89169384f73681c5e65a987c565a0611d32f6
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-04-25 22:50:32 +0900

    Title: 최고의 13위치, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240425/최고의 13위치/best-place-of-13.py
@@ -0,0 +1,12 @@
+import sys
+n = int(input())
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, sys.stdin.readline().split())))
+
+answer = 0
+for i in range(n):
+    for j in range(n-2):
+        answer = max(answer, graph[i][j] + graph[i][j+1] + graph[i][j+2])
+
+print(answer)
