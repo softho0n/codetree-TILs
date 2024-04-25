bfc7d5d0d9d842552d59d37a07ff87c47d6af8fe
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-04-25 22:47:29 +0900

    Title: 일렬로 서있는 소 2, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240425/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(input())
+a = list(map(int, sys.stdin.readline().split()))
+
+answer = 0
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            if a[i] <= a[j] and a[j] <= a[k]:
+                answer += 1
+
+print(answer)
