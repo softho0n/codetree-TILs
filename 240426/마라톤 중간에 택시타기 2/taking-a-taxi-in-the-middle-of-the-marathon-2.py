d853e93867f9ea7caf64919390c25d84e8d80fe2
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-04-26 00:47:49 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 117ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240426/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -0,0 +1,28 @@
+import sys
+
+n = int(input())
+points = []
+
+for _ in range(n):
+    x, y = map(int, sys.stdin.readline().split())
+    points.append((x, y))
+
+def calc(skip_ptn):
+    sy, sx = points[0][0], points[0][0]
+
+    distance = 0
+    for i in range(1, n):
+        if i == skip_ptn:
+            continue
+        else:
+            y, x = points[i][0], points[i][1]
+            distance += abs(y-sy) + abs(x-sx)
+            sy = y
+            sx = x
+    return distance
+
+answer = 1000000000
+for i in range(1, n-1):
+    answer = min(answer, calc(i))
+
+print(answer)
