2e836201259411b21d2c7510bfd64d2372df84fb
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-04-26 16:00:26 +0900

    Title: 두 수의 합, Time: 142ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240426/두 수의 합/sum-of-two-num.py
+++ 240426/두 수의 합/sum-of-two-num.py
@@ -5,14 +5,16 @@ n, k = map(int, sys.stdin.readline().split())
 a = list(map(int, sys.stdin.readline().split()))
 
 for item in a:
-    if not k - item in d:
-        d[k-item] = 1
+    if not item in d:
+        d[item] = 1
     else:
-        d[k-item] += 1
-
+        d[item] += 1
+        
 answer = 0
 for key, v in d.items():
-    if k - key in d:
-        sample = d[k - key]
-        answer += sample * v
+    if key == k - key:
+        answer += (d[key] * d[key] - 1)
+    else:
+        if k - key in d:
+            answer += (d[key] * d[k - key])
 print(answer // 2)
