d4cebf3973ad5952847756818d6d4eab7b666f0e
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-04-26 15:09:35 +0900

    Title: 최고의 13위치 2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240426/최고의 13위치 2/best-place-of-13-2.py
+++ 240426/최고의 13위치 2/best-place-of-13-2.py
@@ -9,7 +9,7 @@ for i in range(n):
     for j in range(n-2):
         for k in range(n):
             for l in range(n-2):
-                if i == k and j == l:
+                if j == l and i == k:
                     continue
                 tmp_sum = graph[i][j] + graph[i][j+1] + graph[i][j+2]
                 tmp_sum += (graph[k][l] + graph[k][l+1] + graph[k][l+2])
