81cdb0d158403695e2454ab128ee6cde70bf870d
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-04-26 12:36:43 +0900

    Title: 씨 오 더블유 2, Time: 146ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240426/씨 오 더블유 2/c-o-w-2.py
@@ -0,0 +1,23 @@
+import sys
+n = int(input())
+s = list(input())
+answer = 0
+v = [False] * n
+
+subset = []
+def go(cnt, idx):
+    if cnt == 3:
+        if subset[0] == 'C' and subset[1] == 'O' and subset[2] == 'W':
+            global answer
+            answer += 1
+        return
+    else:
+        for i in range(n):
+            if v[i] is False:
+                v[i] = True
+                subset.append(s[i])
+                go(cnt + 1, i + 1)
+                subset.pop()
+                v[i] = False
+go(0, 0)
+print(answer)
