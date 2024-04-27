c67e7d9889c7f3cecd09b824d5c066e9a02d10cc
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-04-27 10:26:37 +0900

    Title: 원소의 합이 0, Time: 3814ms, Memory: 25MB, Status: Passed - CodeTree

--- 240427/원소의 합이 0/the-sum-of-the-elements-is-0.py
+++ 240427/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -6,12 +6,20 @@ b = list(map(int, sys.stdin.readline().split()))
 c = list(map(int, sys.stdin.readline().split()))
 d = list(map(int, sys.stdin.readline().split()))
 
+dd = {}
+for item in d:
+    if not item in dd:
+        dd[item] = 1
+    else:
+        dd[item] += 1
+
 answer = 0
 for i in range(n):
     for j in range(n):
         for k in range(n):
-            for p in range(n):
-                s = a[i] + b[j] + c[k] + d[p]
-                if s == 0:
-                    answer += 1
+            K = a[i] + b[j] + c[k]
+            KK = -K
+            if KK in dd:
+                answer += dd[KK]
+
 print(answer)
