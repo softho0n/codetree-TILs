10a6d6de9c171e844a0c2010bb73314903f720c8
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-04-27 10:31:50 +0900

    Title: 순서를 바꾸었을 때 같은 단어 그룹화하기, Time: 176ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240427/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.py
@@ -0,0 +1,22 @@
+import sys
+
+n = int(input())
+d = {}
+
+for _ in range(n):
+    s = list(sys.stdin.readline().rstrip())
+    s = set(s)
+    s = list(s)
+    s.sort()
+    s = ''.join(s)
+
+    if not s in d:
+        d[s] = 1
+    else:
+        d[s] += 1
+
+answer = -1
+for k, v in d.items():
+    if answer <= v:
+        answer = v
+print(answer)
