a71247e1121d989fa023086ccff3b696360b3dc7
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-04-28 10:19:48 +0900

    Title: 구간 중 최대 합, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240428/구간 중 최대 합/max-sum-of-subarray.py
@@ -0,0 +1,11 @@
+import sys
+n, k = map(int, sys.stdin.readline().split())
+a = list(map(int, sys.stdin.readline().split()))
+s = -1
+
+for i in range(n-k):
+    b = 0
+    for p in range(k):
+        b += a[i+p]
+    s = max(s, b)
+print(s)
