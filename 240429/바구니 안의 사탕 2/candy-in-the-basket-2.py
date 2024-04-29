4b026cccc11525467d88f4a70d44b7987796fa94
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-04-29 12:34:05 +0900

    Title: 바구니 안의 사탕 2, Time: 134ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240429/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240429/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,22 +1,16 @@
 import sys
 
 n, k = map(int, sys.stdin.readline().split())
-candy = [0] * 400
+candy = [0] * 10000
 
 for _ in range(n):
     m, x = map(int, sys.stdin.readline().split())
     candy[x] = m
 
 answer = 0
-for i in range(k, 101):
+for i in range(k, 1000):
     tmp_sum = 0
     for j in range(i-k, i+k+1):
-        if j < 0:
-            continue
-        
-        if j > 100:
-            continue
-            
         tmp_sum += candy[j]
     
     answer = max(answer, tmp_sum)
