38cfc449b573e91f5ec15d23f82b804be22fbf3b
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-01 15:40:35 +0900

    Title: 삼각형 만들기, Time: 185ms, Memory: 26MB, Status: Passed - CodeTree

--- 240501/삼각형 만들기/create-triangle.py
+++ 240501/삼각형 만들기/create-triangle.py
@@ -12,23 +12,20 @@ answer = 0
 
 def calc(s):
     global answer
-    if s[0][0] == s[1][0] and s[1][0] != s[2][0]:
-        if s[1][1] != s[2][1]:
-            tmp = abs(s[1][0] - s[2][0]) * abs(s[0][1]-s[1][1])
+    if s[0][0] == s[1][0]:
+        if s[2][1] == s[1][1] or s[2][1] == s[0][1]:
+            tmp = abs(s[0][1] - s[1][1]) * abs(s[2][0] - s[1][0])
             answer = max(answer, tmp)
-            return
     
-    if s[0][0] == s[2][0] and s[2][0] != s[1][0]:
-        if s[2][1] != s[1][1]:
-            tmp = abs(s[2][0] - s[1][0]) * abs(s[0][1]-s[2][1])
+    if s[0][0] == s[2][0]:
+        if s[1][1] == s[0][1] or s[1][1] == s[2][1]:
+            tmp = abs(s[0][1] - s[2][1]) * abs(s[1][0] - s[0][0])
             answer = max(answer, tmp)
-            return
 
-    if s[1][0] == s[2][0] and s[2][0] != s[0][0]:
-        if s[2][1] != s[0][1]:
-            tmp = abs(s[2][0]-s[0][0]) * abs(s[1][1] - s[2][1])
+    if s[1][0] == s[2][0]:
+        if s[0][1] == s[1][1] or s[0][1] == s[2][1]:
+            tmp = abs(s[1][1] - s[2][1]) * abs(s[0][0] - s[1][0])
             answer = max(answer, tmp)
-            return
 
 for i in range(n):
     for j in range(i+1, n):
