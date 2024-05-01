068cf772b827818707aaaac1b0031d6f0475d3d7
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-01 17:02:29 +0900

    Title: 겹치지 않는 선분 2, Time: 129ms, Memory: 25MB, Status: Passed - CodeTree

--- 240501/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 240501/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -9,6 +9,7 @@ for _ in range(n):
 answer = 0
 for i in range(n):
     ix, iy = ptns[i]
+    overlap = False
     for j in range(n):
         if i == j:
             continue
@@ -16,11 +17,15 @@ for i in range(n):
         cx, cy = ptns[j]
         
         if cx <= ix and iy <= cy:
+            overlap = True
             continue
         
         if ix <= cx and cy <= iy:
+            overlap = True
             continue
         
+    
+    if not overlap:
         answer += 1
-
+        
 print(answer)
