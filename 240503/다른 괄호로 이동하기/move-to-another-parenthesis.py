d6329651a42487081294f4f1aa5a73789298d681
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-03 12:53:13 +0900

    Title: 다른 괄호로 이동하기, Time: 512ms, Memory: 29MB, Status: Wrong Answer - CodeTree

--- 240503/다른 괄호로 이동하기/move-to-another-parenthesis.py
+++ 240503/다른 괄호로 이동하기/move-to-another-parenthesis.py
@@ -16,45 +16,54 @@ answer = 0
 
 for i in range(n):
     for j in range(n):
+        INF = 100000000
+        distance = [
+            [INF] * (n + 1)
+            for _ in range(n + 1)
+        ]
+        visited = [
+            [False] * (n + 1)
+            for _ in range(n + 1)
+        ]
+        distance[i][j] = 0
+        visited[i][j] = True
+        q = []
+        heapq.heappush(q, (0, (i, j)))
 
-        for k in range(n):
-            for p in range(n):
+        while q:
+            curd, curtuple = heapq.heappop(q)
+            cy, cx = curtuple
+
+            if distance[cy][cx] < curd:
+                continue
+
+            for z in range(4):
+                ny, nx = cy + dy[z], cx + dx[z]
+
+                if ny < 0 or nx < 0 or ny >= n or nx >= n:
+                    continue
                 
-                INF = 100000000
-                distance = [
-                    [INF] * (n + 1)
-                    for _ in range(n + 1)
-                ]
-                visited = [
-                    [False] * (n + 1)
-                    for _ in range(n + 1)
-                ]
-                distance[i][j] = 0
-                visited[i][j] = True
-                q = []
-                heapq.heappush(q, (0, (i, j)))
-
-                while q:
-                    curd, curtuple = heapq.heappop(q)
-                    cy, cx = curtuple
-
-                    if cy == k and cx == p:
-                        answer = max(answer, curd)
-
-                    for z in range(4):
-                        ny, nx = cy + dy[z], cx + dx[z]
-
-                        if ny < 0 or nx < 0 or ny >= n or nx >= n:
-                            continue
-                        
-                        if visited[ny][nx]:
-                            continue
-                        
-                        if g[cy][cx] != g[ny][nx]:
-                            heapq.heappush(q, (curd + b, (ny, nx)))
-                        else:
-                            heapq.heappush(q, (curd + a, (ny, nx)))
-                        
-                        visited[ny][nx] = True
+                if visited[ny][nx]:
+                    continue
+                
+                if g[cy][cx] != g[ny][nx]:
+                    if curd + b < distance[ny][nx]:
+                        distance[ny][nx] = curd + b
+                        heapq.heappush(q, (curd + b, (ny, nx)))
+                else:
+                    if curd + a < distance[ny][nx]:
+                        distance[ny][nx] = curd + a
+                        heapq.heappush(q, (curd + a, (ny, nx)))
+                
+                visited[ny][nx] = True
+        
+        max_val = 0
+        for i in range(n):
+            for j in range(n):
+                if distance[i][j] == INF:
+                    continue
+                max_val = max(max_val, distance[i][j])
+        
+        answer = max(answer, max_val)
 
 print(answer)
