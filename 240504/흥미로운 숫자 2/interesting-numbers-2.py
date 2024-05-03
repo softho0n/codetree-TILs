dd8b3f958bfd1b748d99ba9326ff2301a4db4185
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-04 08:57:19 +0900

    Title: 흥미로운 숫자 2, Time: 114ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240504/흥미로운 숫자 2/interesting-numbers-2.py
@@ -0,0 +1,16 @@
+import sys
+
+x, y = map(int, sys.stdin.readline().split())
+answer = 0
+
+for i in range(x, y + 1):
+    tmp = list(f"{i}")
+    s = set()
+    for item in tmp:
+        s.add(item)
+    
+    s = list(s)
+    if len(s) == 2:
+        answer += 1
+
+print(answer)
