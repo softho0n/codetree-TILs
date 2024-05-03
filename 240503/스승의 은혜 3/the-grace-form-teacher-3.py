25c689d6c64ee14c10040d2a85a788544243792b
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-03 16:36:53 +0900

    Title: 스승의 은혜 3, Time: 121ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240503/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240503/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -10,9 +10,9 @@ for _ in range(n):
     p, s = map(int, sys.stdin.readline().split())
     info.append((p, s))
 answer = 0
-info.sort()
-for i in range(n):
 
+info.sort(key=lambda x: x[0])
+for i in range(n):
     tmp = info[i][0] // 2 + info[i][1]
     cnt = 1
     for j in range(n):
@@ -26,4 +26,21 @@ for i in range(n):
             else:
                 break
     answer = max(answer, cnt)
+
+info.sort(key=lambda x: x[1])
+for i in range(n):
+    tmp = info[i][0] // 2 + info[i][1]
+    cnt = 1
+    for j in range(n):
+        if i == j:
+            continue
+        else:
+            if tmp + info[j][0] + info[j][1] <= b:
+                tmp += info[j][0]
+                tmp += info[j][1]
+                cnt += 1
+            else:
+                break
+    answer = max(answer, cnt)
+
 print(answer)
