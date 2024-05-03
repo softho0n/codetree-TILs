f88b3348d625405ac3f64afc6e5c04ecfbb9ca0c
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-03 13:24:11 +0900

    Title: 이상한 폭탄 2, Time: 138ms, Memory: 25MB, Status: Passed - CodeTree

--- 240503/이상한 폭탄 2/strange-bomb-2.py
+++ 240503/이상한 폭탄 2/strange-bomb-2.py
@@ -12,15 +12,14 @@ for _ in range(n):
     b.append(int(input()))
 
 answer = -1
-for i in range(n-k+1):
+for i in range(n):
     sample = b[i]
     tmp_answer = -1
-    for j in range(1, k+1):
-        if i + j >= n:
-            continue
-        if b[i+j] == sample:
+    for j in range(i+1, i+k+1):
+        if j >= n:
+            break
+        if sample == b[j]:
             tmp_answer = sample
             break
-    
     answer = max(answer, tmp_answer)
 print(answer)
