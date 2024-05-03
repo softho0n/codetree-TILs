1841c26c6b82a9214691b028e301b189e91a595f
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-03 13:13:08 +0900

    Title: 이상한 폭탄 2, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240503/이상한 폭탄 2/strange-bomb-2.py
@@ -0,0 +1,23 @@
+import sys
+
+
+
+
+
+
+n, k = map(int, sys.stdin.readline().split())
+b = []
+
+for _ in range(n):
+    b.append(int(input()))
+
+answer = -1
+for i in range(n-k):
+    sample = b[i]
+    tmp_answer = -1
+    for j in range(1, k+1):
+        if b[i+j] == sample:
+            tmp_answer = sample
+    
+    answer = max(answer, tmp_answer)
+print(answer)
