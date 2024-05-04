8a27b066d3eaac52e1e7c5863923755b461bc07d
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-04 09:01:33 +0900

    Title: 흥미로운 숫자 2, Time: 463ms, Memory: 26MB, Status: Passed - CodeTree

--- 240504/흥미로운 숫자 2/interesting-numbers-2.py
+++ 240504/흥미로운 숫자 2/interesting-numbers-2.py
@@ -1,4 +1,5 @@
 import sys
+from collections import defaultdict
 
 x, y = map(int, sys.stdin.readline().split())
 answer = 0
@@ -6,12 +7,14 @@ answer = 0
 for i in range(x, y + 1):
     tmp = list(f"{i}")
     s = set()
+    d = defaultdict(int)
     for item in tmp:
         s.add(item)
+        d[item] += 1
     
     s = list(s)
     if len(s) == 2:
-        answer += 1
-        # print(tmp)
-
+        for item in s:
+            if d[item] == 1:
+                answer += 1
 print(answer)
