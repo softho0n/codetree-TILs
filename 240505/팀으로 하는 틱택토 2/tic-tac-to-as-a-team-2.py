59f322bd70de0b09cf85c633f5d1b2454cda2b02
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-05 22:04:07 +0900

    Title: 팀으로 하는 틱택토 2, Time: 206ms, Memory: 24MB, Status: Passed - CodeTree

--- 240505/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 240505/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -8,11 +8,14 @@ for _ in range(3):
     g.append(k)
 
 cnt = 0
+solution = []
 for i in range(3):
     row = g[i]
     s = set(row)
     s = list(s)
-    if len(s) == 2:
+    s.sort()
+    if len(s) == 2 and not s in solution:
+        solution.append(s)
         cnt += 1
 
 for j in range(3):
@@ -22,17 +25,23 @@ for j in range(3):
     
     s = set(tmp)
     s = list(s)
-    if len(s) == 2:
+    s.sort()
+    if len(s) == 2 and not s in solution:
         cnt += 1
+        solution.append(s)
 
 s = set([g[0][0], g[1][1], g[2][2]])
 s = list(s)
-if len(s) == 2:
+s.sort()
+if len(s) == 2 and not s in solution:
     cnt += 1
+    solution.append(s)
 
 s = set([g[0][2], g[1][1], g[2][0]])
 s = list(s)
-if len(s) == 2:
+s.sort()
+if len(s) == 2 and not s in solution:
     cnt += 1
+    solution.append(s)
 
 print(cnt)
