c08b9880aab4d1bcef0c8ab926a56f0618b6fac8
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-06 11:38:55 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 160ms, Memory: 25MB, Status: Passed - CodeTree

--- 240506/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 240506/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -19,8 +19,34 @@ def test(s):
         global answer
         answer = min(answer, len(s))
 
-for i in range(len(n)):
-    test_str = "".join(na[:i+1])
-    test(test_str)
 
-print(answer)
+def test(s, step):
+    cnt = 0
+    for i in range(a):
+        t = na[i:i+step]
+        t = "".join(t)
+
+        if t == s:
+            cnt += 1
+    
+    if cnt >= 2:
+        return True
+    else:
+        return False
+        
+
+for i in range(1, a+1):
+
+    flag = True
+    for j in range(a):
+        test_str = "".join(na[j:j+i])
+        if len(test_str) != i:
+            continue
+        
+        if test(test_str, i):
+            flag = False
+            break
+
+    if flag:
+        print(i)
+        exit()
