d466d13bfe15a13a143e15b729d3a3f0e800dceb
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-07 13:06:51 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 166ms, Memory: 31MB, Status: Passed - CodeTree

--- 240507/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
+++ 240507/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -8,18 +8,11 @@ s, e = 0, 0
 
 while s < n and e < n:
     tmp = a[e] - a[s]
-    # print(s, e)
     if tmp >= 0:
         answer = max(answer, tmp)
         e = e + 1
     else:
         s = s + 1
-
-# for i in range(n):
-#     sample = a[i]
-#     for j in range(i+1, n):
-#         if sample < a[j]:
-#             answer = max(answer, a[j] - sample)
 
 if answer <= 0:
     print(0)
