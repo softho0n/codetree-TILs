fc8cf10b660c84b4b16bbcfa1122631232359fff
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-07 12:10:31 +0900

    Title: 숫자 합치기, Time: 143ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 240507/숫자 합치기/merge-numbers.py
@@ -0,0 +1,25 @@
+import sys
+
+n = int(input())
+a = list(map(int, sys.stdin.readline().split()))
+a.sort()
+
+answer = 0
+
+while True:
+    tmp = []
+    for i in range(0, len(a), 2):
+        if i == len(a) - 1:
+            tmp.append(a[i])
+        else:
+            tmp.append(a[i] + a[i+1])
+            answer += a[i] + a[i+1]
+    
+    if len(tmp) == 1:
+        break
+    
+    a.clear()
+    for item in tmp:
+        a.append(item)
+
+print(answer)
