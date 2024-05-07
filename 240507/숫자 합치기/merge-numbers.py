73db4aa96096e8b06aab1c9935d008ebbe26ab8f
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-07 12:25:32 +0900

    Title: 숫자 합치기, Time: 297ms, Memory: 32MB, Status: Passed - CodeTree

--- 240507/숫자 합치기/merge-numbers.py
+++ 240507/숫자 합치기/merge-numbers.py
@@ -1,23 +1,21 @@
 import sys
+import heapq
 
 n = int(input())
 a = list(map(int, sys.stdin.readline().split()))
-a.sort()
+
+q = []
+for item in a:
+    heapq.heappush(q, item)
 
 answer = 0
-
-
-
 while True:
-    if len(a) == 1:
+    if len(q) == 1:
         break
     
-    a1, a2 = a[0], a[1]
+    a1, a2 = heapq.heappop(q), heapq.heappop(q)
     a3 = a1 + a2
     answer += a3
-
-    a.pop(0)
-    a.pop(0)
-    a.append(a3)
+    heapq.heappush(q, a3)
 
 print(answer)
