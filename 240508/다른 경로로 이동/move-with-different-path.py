b9248df36678582392cb155c05e1bb04f635d60e
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-08 23:49:47 +0900

    Title: 다른 경로로 이동, Time: 410ms, Memory: 32MB, Status: Wrong Answer - CodeTree

---
+++ 240508/다른 경로로 이동/move-with-different-path.py
@@ -0,0 +1,80 @@
+import heapq
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+
+g = [
+    [0] * (n + 1)
+    for _ in range(n + 1)
+]
+
+for _ in range(m):
+    a, b, c = map(int, sys.stdin.readline().split())
+    g[a][b] = c
+    g[b][a] = c
+
+INF = 10000000
+distance = [INF] * (n + 1)
+distance[n] = 0
+
+q = []
+heapq.heappush(q, (0, n))
+
+while q:
+    cd, cp = heapq.heappop(q)
+
+    if distance[cp] < cd:
+        continue
+    
+    for i in range(1, n+1):
+        if g[cp][i] > 0:
+            newd = cd + g[cp][i]
+            if newd < distance[i]:
+                distance[i] = newd
+                heapq.heappush(q, (newd, i))
+    
+x = 1
+path = [x]
+
+while x != n:
+    for i in range(1, n+1):
+        if distance[i] >= INF:
+            continue
+        if i == x:
+            continue
+        
+        if distance[x] == g[x][i] + distance[i]:
+            x = i
+            path.append(x)
+            break
+        
+a_path = []
+for i in range(len(path) - 1):
+    a_path.append((path[i], path[i+1]))
+
+INF = 10000000
+distance = [INF] * (n + 1)
+distance[1] = 0
+
+q = []
+heapq.heappush(q, (0, 1))
+
+while q:
+    cd, cp = heapq.heappop(q)
+
+    if distance[cp] < cd:
+        continue
+    
+    for i in range(1, n+1):
+        if (cp, i) in a_path or (i, cp) in a_path:
+            continue
+        if g[cp][i] > 0:
+            newd = cd + g[cp][i]
+            if newd < distance[i]:
+                distance[i] = newd
+                heapq.heappush(q, (newd, i))
+
+if distance[n] == INF:
+    print(-1)
+else:
+    print(distance[n])
