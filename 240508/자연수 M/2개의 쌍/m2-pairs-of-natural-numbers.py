5598a16068a2ad42193ae96fb844f9de9d043c99
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-08 16:26:01 +0900

    Title: 자연수 M/2개의 쌍, Time: 1073ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240508/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
@@ -0,0 +1,21 @@
+import sys
+import heapq
+
+n = int(sys.stdin.readline().rstrip())
+s = []
+m = 0
+
+for _ in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+    m += a
+    for _ in range(a):
+        s.append(b)
+
+s.sort()
+ans = 1000000001
+# print(s)
+for i in range(0, m // 2):
+    tmp_sum = s[i] + s[m-i-1]
+    ans = min(ans, tmp_sum)
+
+print(ans)
