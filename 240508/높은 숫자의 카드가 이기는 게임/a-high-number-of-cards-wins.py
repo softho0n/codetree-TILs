1f89244abbdab96f32435151b07086fbe01ddd1b
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-08 10:36:49 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240508/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
+++ 240508/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
@@ -1,4 +1,5 @@
 import sys
+from bisect import bisect_right
 
 n = int(input())
 total_number = []
@@ -10,17 +11,36 @@ b_nums = []
 for _ in range(n):
     b_num = int(input())
     b_nums.append(b_num)
-    total_number.remove(b_num)
+    # total_number.remove(b_num)
 
 b_nums.sort()
 total_number.sort()
 answer = 0
 
 for b_num in b_nums:
-    for i in range(len(total_number)):
-        if total_number[i] > b_num:
-            answer += 1
-            total_number.pop(i)
-            break
+    r = bisect_right(total_number, b_num)
+
+    if r >= len(total_number):
+        continue
+    
+    item = total_number[r]
+
+    total_number.pop(r)
+
+    if item > b_num:
+        answer += 1
+
+    # print(total_number, b_num, r, item)
+    # for i in range(len(total_number)):
+    #     if total_number[i] > b_num:
+    #         answer += 1
+    #         total_number.pop(i)
+    #         break
 
 print(answer)
+
+# 1, 2, 3, 4, 5, 6
+
+# 1, 3, 4, 5, 6
+
+# 1, 3, 4, 6
