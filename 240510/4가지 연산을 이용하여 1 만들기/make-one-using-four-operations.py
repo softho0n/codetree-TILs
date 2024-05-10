47dee787fa75df074504a069fe0331fe3b60bb4b
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-10 15:26:18 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 4528ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240510/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -0,0 +1,32 @@
+import sys
+
+
+
+from collections import deque
+import heapq
+
+
+n = int(input())
+v = [False] * (n + 1)
+c = [0] * (n + 1)
+q = []
+heapq.heappush(q, (0, n))
+v[n] = True
+
+answer = 0
+while q:
+    cv, cp = heapq.heappop(q)
+
+    if cp == 1:
+        answer = cv
+        break
+
+    heapq.heappush(q, (cv + 1, cp + 1))
+    heapq.heappush(q, (cv + 1, cp - 1))
+
+    if cp % 2 == 0:
+        heapq.heappush(q, (cv + 1, cp // 2))
+    
+    if cp % 3 == 0:
+        heapq.heappush(q, (cv + 1, cp // 3))
+print(answer)
