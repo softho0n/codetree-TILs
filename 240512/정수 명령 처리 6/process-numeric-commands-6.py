c8a26fac9685dc17cfeb89730ff3e9179dadf749
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-12 02:25:04 +0900

    Title: 정수 명령 처리 6, Time: 242ms, Memory: 29MB, Status: Passed - CodeTree

--- 240512/정수 명령 처리 6/process-numeric-commands-6.py
+++ 240512/정수 명령 처리 6/process-numeric-commands-6.py
@@ -7,19 +7,21 @@ for _ in range(n):
     cmd = list(sys.stdin.readline().split())
     if len(cmd) == 1:
         if cmd[0] == "size":
+            # print(q)
             print(len(q))
         elif cmd[0] == "pop":
             v = heapq.heappop(q)
-            print(v)
+            print(-v)
         elif cmd[0] == "empty":
             if len(q) == 0:
                 print("1")
             else:
                 print("0")
         elif cmd[0] == "top":
-            print(q[-1])
+            print(-q[0])
     else:
         c = cmd[0]
         v = int(cmd[1])
 
-        heapq.heappush(q, v)
+        heapq.heappush(q, -v)
+        # print(q)
