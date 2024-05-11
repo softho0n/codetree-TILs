cb2b6c63e2d84952c2b5a82065671c54bdbdd02a
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-11 11:32:44 +0900

    Title: 두 수의 합, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240511/두 수의 합/sum-of-two-num.py
+++ 240511/두 수의 합/sum-of-two-num.py
@@ -8,12 +8,10 @@ for i in range(n):
     d[a[i]] += 1
 
 ans = 0
-min_v = min(a)
-max_v = max(a)
-for i in range(min_v, max_v + 1):
-    tmp = d[i]
-    tmp2 = d[k-i]
 
-    ans += tmp * tmp2
+for K, v in d.items():
+    if k-K in d:
+        tmp = v * d[k-K]
+        ans += tmp
 
 print(ans // 2)
