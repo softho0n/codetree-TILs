0b2ac2ed03d629f2fb70ddec06eaa60dd3443eb5
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-12 22:18:17 +0900

    Title: 앞에서부터 삭제하기 2, Time: 5070ms, Memory: 32MB, Status: Time Limit Exceed - CodeTree

--- 240512/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 240512/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -5,11 +5,15 @@ n = int(input())
 a = list(map(int, sys.stdin.readline().split()))
 
 answer = 0
-for k in range(1, n-1):
-    q = a[k:]
-    heapq.heapify(q)
-    heapq.heappop(q)
+q = []
+heapq.heappush(q, a[-1])
+for k in range(n-2, 0, -1):
+    heapq.heappush(q, a[k])
+    v = heapq.heappop(q)
+
     avg = sum(q) / len(q)
     answer = max(answer, avg)
+    heapq.heappush(q, v)
+
 
 print("{:.2f}".format(answer))
