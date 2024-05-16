2a9e855e8363d290dc01dc6b15b7154aabeb9d95
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-16 11:48:58 +0900

    Title: 연속한 K개의 숫자, Time: 5052ms, Memory: 32MB, Status: Time Limit Exceed - CodeTree

---
+++ 240516/연속한 K개의 숫자/k-numbers-in-a-row.py
@@ -0,0 +1,51 @@
+import sys
+n, k, b = map(int, sys.stdin.readline().split())
+bl = []
+
+for _ in range(b):
+    bl.append(int(sys.stdin.readline().rstrip()))
+
+total = [item for item in range(0, n+1)]
+origin = []
+for i in range(0, n+1):
+    if i in bl:
+        origin.append(0)
+    else:
+        origin.append(i)
+borigin = []
+for i in range(0, n+1):
+    if i in bl:
+        borigin.append(i)
+    else:
+        borigin.append(0)
+ts = [0] * (n + 1)
+os = [0] * (n + 1)
+bs = [0] * (n + 1)
+for i in range(1, n + 1):
+    ts[i] = ts[i-1] + total[i]
+for i in range(1, n + 1):
+    os[i] = os[i-1] + origin[i]
+for i in range(1, n + 1):
+    bs[i] = bs[i-1] + borigin[i]
+# print(total)
+# print(origin)
+# print(borigin)
+
+# print(ts)
+# print(os)
+# print(bs)
+answer = 10000000
+for i in range(n+1):
+    if i+k <= n:
+        tmp1 = ts[i+k] - ts[i]
+        tmp2 = os[i+k] - os[i]
+        tmp3 = bs[i+k] - bs[i] 
+
+        if tmp1 == tmp2 + tmp3:
+            tmp_answer = 0
+            for j in range(i+1, i+1+k):
+                if borigin[j] != 0:
+                    tmp_answer += 1
+            answer = min(answer, tmp_answer)
+            # print(i, tmp1, tmp2, tmp3, tmp_answer)
+print(answer)
