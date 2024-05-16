d1a051461af0f2d7f72b37e9a622f15679554e95
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-16 12:25:54 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 219ms, Memory: 43MB, Status: Wrong Answer - CodeTree

--- 240516/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.py
+++ 240516/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.py
@@ -2,14 +2,16 @@ import sys
 
 n, q = map(int, sys.stdin.readline().split())
 p = list(map(int, sys.stdin.readline().split()))
+arr = [0] * (1000000 + 1)
+
+for item in p:
+    arr[item] = 1
+
 ps = [0] * (1000000 + 1)
 
 for i in range(1, 1000000 + 1):
-    if i in p:
-        ps[i] = ps[i-1] + 1
-    else:
-        ps[i] = ps[i-1]
-
+    ps[i] = ps[i-1] + arr[i]
+    
 # print(ps)
 for _ in range(q):
     a, b = map(int, sys.stdin.readline().split())
