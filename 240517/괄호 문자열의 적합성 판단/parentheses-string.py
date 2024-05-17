341bae4fb84db6afbd092bf7fb877dd2f48d5b56
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-17 18:41:06 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 160ms, Memory: 24MB, Status: Passed - CodeTree

--- 240517/괄호 문자열의 적합성 판단/parentheses-string.py
+++ 240517/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -6,7 +6,11 @@ for i in range(len(s)):
     if s[i] == "(":
         a.append("(")
     else:
+        if len(a) == 0:
+            print("No")
+            exit(0)
         a.pop()
+
 if len(a) >= 1:
     print("No")
 else:
