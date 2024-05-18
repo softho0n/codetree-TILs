7fda91ab137f94cca1d3bd522e234982d3fd3d31
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-18 23:53:44 +0900

    Title: 트리 판별, Time: 115ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240518/트리 판별/tree-identification.py
+++ 240518/트리 판별/tree-identification.py
@@ -15,24 +15,41 @@ def union(parent, a, b):
         parent[a] = fb
 
 m = int(input())
-edges = [
-    []
-    for _ in range(m+1)
-]
 
 parent = [0 for _ in range(10001)]
+visited = [False for _ in range(10001)]
 for i in range(10001):
     parent[i] = i
 
+edges = [
+    []
+    for _ in range(10001)
+]
+start = -1
 for _ in range(m):
     a, b = map(int, sys.stdin.readline().split())
-    # edges[a].append(b)
-    # edges[b].append(a)
-
+    edges[a].append(b)
+    edges[b].append(a)
+    start = a
+    visited[a] = True
+    visited[b] = True
     if find_parent(parent, a) != find_parent(parent, b):
         union(parent, a, b)
     else:
         print(0)
         exit(0)
 
+dfs_visited = [False for _ in range(10001)]
+dfs_visited[start] = True
+def go(s):
+    for next_p in edges[s]:
+        if dfs_visited[next_p] is False:
+            dfs_visited[next_p] = True
+            go(next_p)
+go(start)
+
+for i in range(1, 10001):
+    if visited[i] != dfs_visited[i]:
+        print(0)
+        exit(0)
 print(1)
