5c70e903178d88bd48af0761c5252b2d0174b70b
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-18 21:43:35 +0900

    Title: 트리 노드 제거, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240518/트리 노드 제거/remove-tree-node.py
@@ -0,0 +1,30 @@
+import sys
+
+n = int(input())
+a = list(map(int, sys.stdin.readline().split()))
+parents = [0 for _ in range(n)]
+childs = [
+    []
+    for _ in range(n)
+]
+
+for idx, item in enumerate(a):
+    parents[idx] = item
+    
+    if item != -1:
+        childs[item].append(idx)
+
+d = int(input())
+answer = 0
+
+def go(s):
+    parents[s] = -2
+    for next_pos in childs[s]:
+        go(next_pos)
+
+go(d)
+for i in range(n):
+    if len(childs[i]) == 0 and parents[i] != -2:
+        answer += 1
+
+print(answer)
