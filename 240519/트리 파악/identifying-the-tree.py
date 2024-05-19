8ba4db8b9cbeb138d584673fbc69c61bf576088d
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-19 22:54:40 +0900

    Title: 트리 파악, Time: 1012ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240519/트리 파악/identifying-the-tree.py
+++ 240519/트리 파악/identifying-the-tree.py
@@ -16,20 +16,20 @@ distance = [-1 for _ in range(n+1)]
 distance[0] = 0
 distance[1] = 0
 
-def go(s, dist):
+def go(s):
     visit = False
-    new_dist = dist + 1
+    new_dist = distance[s] + 1
 
     for new_pos in edges[s]:
         if distance[new_pos] == -1:
             distance[new_pos] = new_dist
-            go(new_pos, new_dist)
+            go(new_pos)
             visit = True
     
     if visit:
         distance[s] = 0
 
-go(1, 0)
+go(1)
 total_length = 0
 for i in range(2, n+1):
     total_length += distance[i]
