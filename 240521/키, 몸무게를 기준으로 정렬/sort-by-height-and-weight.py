c2c340a3cee05b74265053b10f3a853c47ea2c0d
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-21 10:12:30 +0900

    Title: 키, 몸무게를 기준으로 정렬, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240521/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.py
@@ -0,0 +1,9 @@
+import sys
+n = int(input())
+p = []
+for _ in range(n):
+    n, h, w = sys.stdin.readline().split()
+    p.append((h, int(h), int(w)))
+p.sort(key=lambda x: (x[1]))
+for n, h, w in p:
+    print(n, h, w)
