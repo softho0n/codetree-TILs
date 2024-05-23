59080c743c61ed89628ebe72c1b295cfdd76039b
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-23 12:24:55 +0900

    Title: 거꾸로 순열, Time: 244ms, Memory: 29MB, Status: Passed - CodeTree

---
+++ 240523/거꾸로 순열/backward-permutation.py
@@ -0,0 +1,20 @@
+import sys
+n = int(input())
+subset = []
+visited = [False for _ in range(n + 1)]
+def go(cnt):
+    if cnt == n:
+        print(*subset)
+        return
+    else:
+        for i in range(n, 0, -1):
+            if visited[i]:
+                continue
+            
+            visited[i] = True
+            subset.append(i)
+            go(cnt + 1)
+            subset.pop()
+            visited[i] = False
+
+go(0)
