d2cf7f8911b67e6fc69e8d08b6c5aea8e100c39c
Seunghun Shin <42256738+softho0n@users.noreply.github.com>
2024-05-26 18:51:33 +0900

    Title: 간선 순회, Time: 1069ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240526/간선 순회/touring-the-line.py
+++ 240526/간선 순회/touring-the-line.py
@@ -1,5 +1,5 @@
 import sys
-
+import heapq
 
 
 sys.setrecursionlimit(70000)
@@ -15,8 +15,7 @@ for _ in range(n-1):
     tree[b].append((a, c))
 
 visited = [False for _ in range(n + 1)]
-distance = [0 for _ in range(n + 1)]
-real_distance = [0 for _ in range(n + 1)]
+q = []
 def dfs(x, d, rd):
     for next_pos, next_val in tree[x]:
         if visited[next_pos]:
@@ -24,61 +23,64 @@ def dfs(x, d, rd):
         
         new_dist = d + 1
         new_rd = rd + next_val
-        distance[next_pos] = new_dist
-        real_distance[next_pos] = new_rd
+        
+        heapq.heappush(q, (-new_dist, new_rd, next_pos))
         visited[next_pos] = True
         dfs(next_pos, new_dist, new_rd)
 
 visited[2] = True
 dfs(2, 0, 0)
+visited = [False for _ in range(n + 1)]
 
-max_dist = max(distance)
-candidate = []
-
-for i in range(1, n + 1):
-    if distance[i] == max_dist:
-        candidate.append((real_distance[i], i))
-
-candidate.sort()
-candidate = candidate[:1]
+Z, A, POS = heapq.heappop(q)
+visited[POS] = True
+q.clear()
 def dfs_v2(x, d, rd):
     for next_pos, next_val in tree[x]:
         if visited[next_pos]:
             continue
         next_d = d + 1
         next_rd = rd + next_val
+        # print(x, next_pos, rd, next_val, next_d)
         visited[next_pos] = True
-        distance[next_pos] = next_d
-        real_distance[next_pos] = next_rd
+        heapq.heappush(q, (-next_d, next_rd, next_pos))
+        # distance[next_pos] = next_d
+        # real_distance[next_pos] = next_rd
         dfs_v2(next_pos, next_d, next_rd)
-
-answer = 1000000
-for _, can in candidate:
-
-    visited = [False for _ in range(n + 1)]
-    visited[can] = True
-
-    distance = [0 for _ in range(n + 1)]
-    real_distance = [0 for _ in range(n + 1)]
-
-    dfs_v2(can, 0, 0)
-
-    second_candidate = []
-    second_max = max(distance)
-    for i in range(n + 1):
-        if distance[i] == second_max:
-            second_candidate.append(i)
+# print(POS)
+dfs_v2(POS, 0, 0)
+level, tmp, _ = heapq.heappop(q)
+answer = 10000000
+if tmp % d == 0:
+    answer = min(answer, tmp // d)
+else:
+    answer = min(answer, (tmp // d) + 1)
     
-    max_length = []
-    for sec_can in second_candidate:
-        max_length.append(real_distance[sec_can])
-
-    max_length.sort()
-    tmp = max_length[0]
-
-    if tmp % d == 0:
-        answer = min(answer, tmp // d)
-    else:
-        answer = min(answer, (tmp // d) + 1)
-    # print(can, max_length)
 print(answer)
+# answer = 1000000
+# for _, can in candidate:
+
+#     visited = [False for _ in range(n + 1)]
+#     visited[can] = True
+
+#     distance = [0 for _ in range(n + 1)]
+#     real_distance = [0 for _ in range(n + 1)]
+
+#     dfs_v2(can, 0, 0)
+
+#     second_candidate = []
+#     second_max = max(distance)
+#     for i in range(n + 1):
+#         if distance[i] == second_max:
+#             second_candidate.append(i)
+    
+#     max_length = []
+#     for sec_can in second_candidate:
+#         max_length.append(real_distance[sec_can])
+
+#     max_length.sort()
+#     tmp = max_length[0]
+
+
+#     # print(can, max_length)
+# print(answer)
